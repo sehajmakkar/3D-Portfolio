ca551d22fa000f25843c7ea09bf53860f55ed984
sehajmakkar <codewithsehaj@gmail.com>
2024-09-08 02:05:01 +0530

    some practice

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,11 @@
+import { Canvas } from '@react-three/fiber';
+
+const App = () => {
+  return (
+    <Canvas style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      Hello THREE JS
+    </Canvas>
+  )
+}
+
+export default App
